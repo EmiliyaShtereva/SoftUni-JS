22eaef2a38ebc2ee2730258c522f460cf949457f
EmiliyaShtereva <123276538+EmiliyaShtereva@users.noreply.github.com>
2023-09-30 10:17:29 +0300

    mongodb and mongoose workshop init app

---
+++ JSBackEnd/08.WorkshopMongoDBAndMongoose/04. Cubicle-Workshop-Part-2-Resources/src/controllers/cubeController.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const cubeService = require('../services/cubeService.js');
+
+router.get('/create', (req, res) => {
+    res.render('create');
+});
+
+router.post('/create', async (req, res) => {
+    const {name, description, imageUrl, difficultyLevel} = req.body;
+    await cubeService.create({name, description, imageUrl, difficultyLevel: Number(difficultyLevel)});
+    res.redirect('/');
+});
+
+router.get('/:cubeId/details', async (req, res) => {
+    const {cubeId} = req.params;
+    const cube = await cubeService.getSingleCube(cubeId);
+
+    if (!cube) {
+        res.redirect('/404');
+        return;
+    }
+
+    res.render('details', {...cube});
+});
+
+module.exports = router;
