b03e3092d269f657b55495e17a085cd39462d24f
EmiliyaShtereva <123276538+EmiliyaShtereva@users.noreply.github.com>
2023-10-20 14:13:10 +0300

    added authentication

--- JSBackEnd/12.13.ExamPreparation/12.friendlyWorldResources/FriendlyWorld/jsFiles/configs.js
+++ JSBackEnd/12.13.ExamPreparation/12.friendlyWorldResources/FriendlyWorld/jsFiles/configs.js
@@ -4,12 +4,15 @@ const handlebars = require('express-handlebars');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 
+const {auth} = require('./authMiddleware.js')
+
 const URL = 'mongodb://localhost:27017/friendly-world';
 
 exports.expressConfig = (app) => {
     app.use(express.static(path.resolve(__dirname, '../static')));
     app.use(express.urlencoded({extended: false}));
     app.use(cookieParser());
+    app.use(auth);
 };
 
 exports.handlebarsConfig = (app) => {
