808f30cf1f2f99c67f53f6152a7cc1d5f7531426
EmiliyaShtereva <123276538+EmiliyaShtereva@users.noreply.github.com>
2023-10-20 14:37:30 +0300

    login and logout functionality

--- JSBackEnd/12.13.ExamPreparation/12.friendlyWorldResources/FriendlyWorld/jsFiles/controllers/usersController.js
+++ JSBackEnd/12.13.ExamPreparation/12.friendlyWorldResources/FriendlyWorld/jsFiles/controllers/usersController.js
@@ -6,6 +6,20 @@ router.get('/login', (req, res) => {
     res.render('user/login');
 });
 
+router.post('/login', async (req, res) => {
+    const {email, password} = req.body;
+
+    try {
+        const token = await userService.login(email, password);
+        res.cookie('token', token, {httpOnly: true});
+        console.log(req.body);
+        res.redirect('/');
+    } catch(error) {
+        const errorMessages = extractErrorMsgs(error).join('\n');
+        res.status(404).render('user/login', {errorMessages});
+    }
+});
+
 router.get('/register', (req, res) => {
     res.render('user/register');
 });
@@ -24,7 +38,9 @@ router.post('/register', async (req, res) => {
 });
 
 router.get('/logout', (req, res) => {
+    res.clearCookie('token');
     res.redirect('/');
 });
+
 
 module.exports = router;
--- JSBackEnd/12.13.ExamPreparation/12.friendlyWorldResources/FriendlyWorld/jsFiles/survices/userService.js
+++ JSBackEnd/12.13.ExamPreparation/12.friendlyWorldResources/FriendlyWorld/jsFiles/survices/userService.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const User = require("../models/User.js");
 const jwt = require('../jwt.js');
 
@@ -6,6 +7,25 @@ const SECRET = 'c6a65234-9214-4822-bd40-ce841bf1e927';
 exports.register = async (userData) => {
     const {password} = userData;
     const user = await User.create(userData);
+
+    const isValid = await bcrypt.compare(password, user.password);
+
+    if (!isValid) {
+        throw new Error('Invalid email or password!');
+    };
+
+    const payload = {_id: user._id, email: user.email};
+    const token = await jwt.sign(payload, SECRET, {expiresIn: '3d'});
+
+    return token;
+}
+
+exports.login = async (email, password) => {
+    const user = await User.findOne({email});
+
+    if (!user) {
+        throw new Error('Invalid email or password!');
+    };
 
     const isValid = await bcrypt.compare(password, user.password);
 
